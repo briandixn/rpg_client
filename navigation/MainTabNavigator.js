ee0b2b4130e0a982c642790445cb9462c86fb48d
James Stuckey <jstuckey@rutgers.edu>
2019-04-01 12:31:06 -0400

    Add dungeon to bottom tab bar, needs icon

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,30 +1,44 @@
 import React from 'react';
 import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import {
+    createStackNavigator,
+    createBottomTabNavigator
+} from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import DungeonScreen from '../screens/DungeonScreen';
 
 const HomeStack = createStackNavigator({
-  Home: HomeScreen,
-  Dungeons: DungeonScreen
+    Home: HomeScreen
 });
 
 HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
+    tabBarLabel: 'Home',
+    tabBarIcon: ({ focused }) => (
+        <TabBarIcon
+            focused={focused}
+            name={
+                Platform.OS === 'ios'
+                    ? `ios-information-circle${focused ? '' : '-outline'}`
+                    : 'md-information-circle'
+            }
+        />
+    )
 };
 
+const DungeonStack = createStackNavigator({
+    Dungeons: DungeonScreen
+});
+
+DungeonStack.navigationOptions = {
+	tabBarLabel: 'Dungeons',
+	tabBarIcon: ({ focused }) => (
+		<TabBarIcon focused={focused}/>
+	)
+}
+
 export default createBottomTabNavigator({
-  HomeStack
+	HomeStack,
+	DungeonStack
 });
