5f878a1ad3eccc45f7ce836f0d331366ddacaa3a
James Stuckey <jstuckey@rutgers.edu>
2019-04-01 12:05:29 -0400

    Format

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,193 +1,183 @@
 import React from 'react';
 import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
+    Image,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
 
 export default class HomeScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
+    static navigationOptions = {
+        header: null
+    };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          
+    render() {
+        return (
+            <View style={styles.container}>
+                <ScrollView
+                    style={styles.container}
+                    contentContainerStyle={styles.contentContainer}
+                >
+                    <View style={styles.mapSection}>
+                        <Text style={styles.storeSection}>
+                            <Image
+                                style={styles.storeIcon}
+                                source={require('../assets/images/sword.png')}
+                            />
+                            Weapons shop
+                        </Text>
 
-          <View style={styles.mapSection}>
+                        <Text style={styles.storeSection}>Recruit heros</Text>
 
-          <Text style={styles.storeSection}>
-          <Image style={styles.storeIcon}source={require('../assets/images/sword.png')}/>
-          weapons shop!!!
-          </Text>
+                        <Text style={styles.storeSection}>Buy items</Text>
+                    </View>
 
-
-          <Text style={styles.storeSection}>
-          Recruit heros
-          </Text>
-
-          <Text style={styles.storeSection}>
-          Buy items
-          </Text>
-          </View>
-
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-
-          </View>
-
-
-        </ScrollView>
-
-
-      </View>
-    );
-  }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled {learnMoreButton}
-        </Text>
-
-
-      );
+                    <View style={styles.getStartedContainer}>
+                        {this._maybeRenderDevelopmentModeWarning()}
+                    </View>
+                </ScrollView>
+            </View>
+        );
     }
-  }
 
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
+    _maybeRenderDevelopmentModeWarning() {
+        if (__DEV__) {
+            const learnMoreButton = (
+                <Text
+                    onPress={this._handleLearnMorePress}
+                    style={styles.helpLinkText}
+                >
+                    Learn more
+                </Text>
+            );
 
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
+            return (
+                <Text style={styles.developmentModeText}>
+                    Development mode is enabled {learnMoreButton}
+                </Text>
+            );
+        }
+    }
+
+    _handleLearnMorePress = () => {
+        WebBrowser.openBrowserAsync(
+            'https://docs.expo.io/versions/latest/guides/development-mode'
+        );
+    };
+
+    _handleHelpPress = () => {
+        WebBrowser.openBrowserAsync(
+            'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+        );
+    };
 }
 
 const styles = StyleSheet.create({
-  container: {
+    container: {
+        backgroundColor: '#fff'
+    },
+    mapSection: {
+        flex: 1,
+        flexDirection: 'row',
+        margin: 1
+    },
+    storeIcon: {
+        margin: 3
+    },
 
-    backgroundColor: '#fff',
-
-  },
-  mapSection: {
-    flex: 1,
-    flexDirection: 'row',
-    margin: 1,
-
-  },
-  storeIcon:{
-    margin:3,
-  },
-
-  storeSection:{
-    width: 80,
-    height: 100,
-    backgroundColor: '#D4D4D4',
-    margin: 10,
-    borderRadius: 5,
-
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+    storeSection: {
+        width: 80,
+        height: 100,
+        backgroundColor: '#D4D4D4',
+        margin: 10,
+        borderRadius: 5
+    },
+    developmentModeText: {
+        marginBottom: 20,
+        color: 'rgba(0,0,0,0.4)',
+        fontSize: 14,
+        lineHeight: 19,
+        textAlign: 'center'
+    },
+    contentContainer: {
+        paddingTop: 30
+    },
+    welcomeContainer: {
+        alignItems: 'center',
+        marginTop: 10,
+        marginBottom: 20
+    },
+    welcomeImage: {
+        width: 100,
+        height: 80,
+        resizeMode: 'contain',
+        marginTop: 3,
+        marginLeft: -10
+    },
+    getStartedContainer: {
+        alignItems: 'center',
+        marginHorizontal: 50
+    },
+    homeScreenFilename: {
+        marginVertical: 7
+    },
+    codeHighlightText: {
+        color: 'rgba(96,100,109, 0.8)'
+    },
+    codeHighlightContainer: {
+        backgroundColor: 'rgba(0,0,0,0.05)',
+        borderRadius: 3,
+        paddingHorizontal: 4
+    },
+    getStartedText: {
+        fontSize: 17,
+        color: 'rgba(96,100,109, 1)',
+        lineHeight: 24,
+        textAlign: 'center'
+    },
+    tabBarInfoContainer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        ...Platform.select({
+            ios: {
+                shadowColor: 'black',
+                shadowOffset: { height: -3 },
+                shadowOpacity: 0.1,
+                shadowRadius: 3
+            },
+            android: {
+                elevation: 20
+            }
+        }),
+        alignItems: 'center',
+        backgroundColor: '#fbfbfb',
+        paddingVertical: 20
+    },
+    tabBarInfoText: {
+        fontSize: 17,
+        color: 'rgba(96,100,109, 1)',
+        textAlign: 'center'
+    },
+    navigationFilename: {
+        marginTop: 5
+    },
+    helpContainer: {
+        marginTop: 15,
+        alignItems: 'center'
+    },
+    helpLink: {
+        paddingVertical: 15
+    },
+    helpLinkText: {
+        fontSize: 14,
+        color: '#2e78b7'
+    }
 });
