be859b3d8a18fe42ea04613f69027ea95e844d0c
James Stuckey <jstuckey@rutgers.edu>
2019-04-01 13:22:55 -0400

    Test button

--- screens/DungeonScreen.js
+++ screens/DungeonScreen.js
@@ -7,17 +7,43 @@ import {
     StyleSheet,
     Text,
     TouchableOpacity,
-    View
+    View,
+    Button
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
 export default class DungeonScreen extends React.Component {
+    constructor(props) {
+        super(props);
 
-	render() {
-		return <View>
-			<Text>Placeholder</Text>
-			<Text>Placeholder</Text>
-			<Text>Placeholder</Text>
-		</View>
-	}
+        this.handlePress = this.handlePress.bind(this);
+    }
+
+    handlePress() {
+        alert('Button pressed');
+    }
+
+    render() {
+        return (
+            <ScrollView
+                style={styles.container}
+                contentContainerStyle={styles.contentContainer}
+            >
+                <Text>Placeholder</Text>
+                <Text>Placeholder</Text>
+                <Text>Placeholder</Text>
+                <Button title="Press Me" onPress={this.handlePress} />
+            </ScrollView>
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff'
+    },
+
+    contentContainer: {
+        marginTop: 30
+	}
+});
